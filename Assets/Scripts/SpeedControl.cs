f93df3453ef1c5f7ea130766284b53d41203d06f
mildmojo <mildmojo@users.noreply.github.com>
2015-03-19 20:47:45 -0400

    Adds arrow key velocity control.

--- Assets/Scripts/SpeedControl.cs
+++ Assets/Scripts/SpeedControl.cs
@@ -17,35 +17,34 @@ public class SpeedControl : MonoBehaviour {
 
     public float currentSpeed;
     public float accelRate;
-	// Use this for initialization
-	void Start () 
+    // Use this for initialization
+    void Start ()
     {
-        LeftTurns = new KeyCombo[] {
-      new KeyCombo("Horizontal", "Vertical", new string[] {"up", "left", "down"}, TURN_INPUTS),
-      new KeyCombo("Horizontal", "Vertical", new string[] {"down", "right", "up"}, TURN_INPUTS)
-    };
+      LeftTurns = new KeyCombo[] {
+        new KeyCombo("Horizontal", "Vertical", new string[] {"up", "left", "down"}, TURN_INPUTS),
+        new KeyCombo("Horizontal", "Vertical", new string[] {"down", "right", "up"}, TURN_INPUTS)
+      };
 
-        RightTurns = new KeyCombo[] {
-      new KeyCombo("Horizontal", "Vertical", new string[] {"up", "right", "down"}, TURN_INPUTS),
-      new KeyCombo("Horizontal", "Vertical", new string[] {"down", "left", "up"}, TURN_INPUTS)
-    };
-	
-	}
-	
-	// Update is called once per frame
-	void Update () 
+      RightTurns = new KeyCombo[] {
+        new KeyCombo("Horizontal", "Vertical", new string[] {"up", "right", "down"}, TURN_INPUTS),
+        new KeyCombo("Horizontal", "Vertical", new string[] {"down", "left", "up"}, TURN_INPUTS)
+      };
+
+      foreach (KeyCombo turn in RightTurns.Concat(LeftTurns))
+      {
+          turn.HorizontalAxis = "Horizontal";
+          turn.VerticalAxis = "Vertical";
+      }
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
-        foreach (KeyCombo turn in RightTurns.Concat(LeftTurns))
-        {
-            turn.HorizontalAxis = "Horizontal";
-            turn.VerticalAxis = "Vertical";
-        }
-
-        if (RightTurns.Any(turn => turn.Check()))
+        if (RightTurns.Any(turn => turn.Check()) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             currentSpeed -= accelRate;
         }
-        if (LeftTurns.Any(turn => turn.Check()))
+        if (LeftTurns.Any(turn => turn.Check()) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             currentSpeed += accelRate;
         }
@@ -55,8 +54,8 @@ public class SpeedControl : MonoBehaviour {
         Time.timeScale = currentSpeed;
         audioSourceOne.pitch = currentSpeed;
         audioSourceTwo.pitch = currentSpeed;
-	
-	}
+
+    }
 
     void FixedUpdate()
     {
